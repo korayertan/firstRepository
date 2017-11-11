36244088c984ea65daae29aedd0f60214f746bca
korayertan <macbookpro@2015s-MacBook-Pro.local>
2017-11-10 21:55:51 -0500

    versiyon 2 den alindi

---
+++ koray_test V2.java
@@ -0,0 +1,32 @@
+package com.shiftedtech.qa.algorithm;
+
+public class koray {
+    public static void main(String[] args) {
+
+
+        String newArray[] = {"K", "o", "r", "a", "y"};
+
+
+        for (int i = newArray.length - 1; i>=0 ; i--) {
+            //for (int i = newArray.length; i>=0 ; i--)
+            System.out.println(newArray[i]);
+        }
+    }
+}
+
+/*versiyon 2*/
+/*
+
+package com.shiftedtech.qa.algorithm;
+
+public class KhadezaAlgorithmPractice {
+
+    public static String practiceProblem(String str){
+        String reverse="";
+        for (int i=str.length()-1; i>=0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+        return reverse;
+        }
+}
+ */
